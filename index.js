57257d421499bef8a66c7eb475450338b4c990fd
Jose Germán Martínez <jgermanmm@gmail.com>
2025-02-17 12:18:46 -0300

    Llamar a la función dbConnection en el inicio del servidor y exportar la aplicación

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = require("./src/app/app"); // importamos app
 const {dbConnection} = require("./src/database/conexion"); // importamos dbConnection
 const port = 8080;
 
-
+dbConnection(); // llamamos a la función dbConnection
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
-dbConnection(); // llamamos a la función dbConnection
+module.exports = app;
